50902907dce2f2ddf8d5c60e4936ccdd7859c0ce
Qiming YE <34636290+kimiye1989@users.noreply.github.com>
2022-10-28 13:10:48 +0100

    Plotted the whole year freq
    Plotted the whole year freq

--- methods.py
+++ methods.py
@@ -3,9 +3,9 @@ import random
 import json
 
 import matplotlib.pyplot as plt
+import matplotlib.ticker as mtick
 
 from functions import read_json, Write_to_json
-
 
 def Single_keyword_Sort_Filter_By_Year(file_dir, display_threshold):
     new_dic = {}
@@ -45,7 +45,6 @@ def Single_keyword_All_Year(file_dir):
                 All_dic.update({k:new_count})
     return All_dic, sum_num
 
-
 def Single_keyword_Sort_Filter_All_Year(file_dir, display_threshold):
     # Acquire the comprehensive dictionary of sampled data
     whole_dict, sum_count = Single_keyword_All_Year(file_dir)
@@ -53,7 +52,6 @@ def Single_keyword_Sort_Filter_All_Year(file_dir, display_threshold):
     # Descendingly sort the whole keyword dictionary by their frequences, Pick top 20 to form a new dictionary
     sorted_whole_dic = {k: v for k, v in sorted(whole_dict.items(), key=lambda item: item[1], reverse = True)[:int(display_threshold)]}
     return sorted_whole_dic, sum_count
-
 
 def Add_to_tabu_json(word, json_file):
     d = read_json(json_file)
@@ -68,7 +66,6 @@ def Add_to_tabu_json(word, json_file):
         d.update({'Tabu':dic})
         print("Succeed! The new word '%s' has been add to excluding list"%word)
     Write_to_json(json_file, d)
-
 
 def Add_to_replace_json(word, main_word, json_file):
     d = read_json(json_file)
@@ -90,4 +87,54 @@ def Add_to_replace_json(word, main_word, json_file):
 
     Write_to_json(json_file, d)
 
+def Plot_Whole_Year_Barchart(file_dir, display_threshold_all_years):
+
+    whole_dic, original_occur = Single_keyword_Sort_Filter_All_Year(file_dir, display_threshold_all_years)
+
+    X_lis = whole_dic.keys()
+    Y_lis_temp = whole_dic.values()
+
+    # Make sure every accum num is a positive integer
+    Y_lis = [int(max(y,0)) for y in Y_lis_temp]
+
+    Y_pesudo = [y/sum(Y_lis) for y in Y_lis_temp]
+    Y_perc_lis = [y/int(original_occur) for y in Y_lis]
+
+    fig = plt.figure(figsize=(15,7))
+    ax1 = fig.add_axes([0, 0, 1, 1])
+    ax2 = ax1.twinx()
+    ax1.set(facecolor = "whitesmoke")
+    ax1.grid(axis='both', color='lightgray')
+
+    ax1.bar(X_lis, Y_lis, align='center', color='gray', label='Cumulative Occurences', alpha = 0.8)
+    ax2.plot(X_lis, Y_pesudo, linewidth=2, color='blue', label='Percentiles (to Top %d Keywords)'%display_threshold_all_years) 
+    ax2.plot(X_lis, Y_perc_lis, linewidth=2, color='red', label='Percentiles (to All Keywords)')
+
+    ax2.scatter(X_lis, Y_pesudo, s=15, color='k')
+    ax2.scatter(X_lis, Y_perc_lis, s=15, color='k')
+
+    ax2.axvline(x=9, c='k', lw=1.5, linestyle='dashed', alpha = 0.6)
+    ax2.axvline(x=19, c='k', lw=1.5, linestyle='dashed', alpha = 0.6)
+    ax2.axvline(x=29, c='k', lw=1.5, linestyle='dashed', alpha = 0.6)
+    ax2.axvline(x=39, c='k', lw=1.5, linestyle='dashed', alpha = 0.6, label='Per 10 Keywords')
+    #ax1.annotate(10, max(Y_lis), (10, max(Y_lis)), c='k', fontsize = 12)
+
+    ax1.text(-1, max(Y_lis)+0.5, str(max(Y_lis)), horizontalalignment='center', color = 'k',verticalalignment='center', fontsize = 16)
+    ax2.text(-1, max(Y_pesudo)-0.007, "{0:.0%}".format(max(Y_pesudo)), horizontalalignment='center', color = 'blue',verticalalignment='center',fontsize = 16)
+    ax2.text(-1, max(Y_perc_lis), "{0:.0%}".format(max(Y_perc_lis)), horizontalalignment='center', color = 'red', verticalalignment='center',fontsize = 16)
     
+    ax1.set_xlabel('Keywords', fontsize=18)
+    ax1.set_xticklabels(X_lis, rotation=90)
+    ax2.yaxis.set_major_formatter(mtick.PercentFormatter(xmax=1, decimals=None, symbol='%', is_latex=False))
+
+    ax1.set_ylabel("Accumulative Occurences", fontsize=15, labelpad=12)
+    ax2.set_ylabel("Percentile Occurences", fontsize=15, labelpad=12)
+
+    ax1.set_title('Frequences of Top %d Keywords of Year Spectrum (1923-1974)'%display_threshold_all_years, fontsize=18, y=1.06)
+
+    plt.legend(loc='upper right', fontsize=16, ncol=1)
+
+    plt.tight_layout()
+
+    plt.savefig('./Outputs/All_year_single_word_frequencies.png', dpi=300, bbox_inches='tight')
+    plt.show()
