416524dedde37cf77d7224b87fb3036f1abadfaa
Qiming YE <34636290+kimiye1989@users.noreply.github.com>
2022-10-28 12:35:44 +0100

    Added a plot function
    Added a plot function

--- functions.py
+++ functions.py
@@ -1,7 +1,4 @@
-import matplotlib.pyplot as plt
 import json
-
-import pandas as pd
 
 def read_json(file_name):
     with open(file_name, 'r') as f:
@@ -11,11 +8,4 @@ def read_json(file_name):
 def Write_to_json(file_name, data):
     with open(file_name, 'w', encoding='utf-8') as f:
         json.dump(data, f, ensure_ascii=False, indent=4)
-
-def Add_to_tabu_json(word, json_file):
-    d = read_json(file_name)
-    d.update({word})
-
-def Add_to_replace_json(word, main_word, json_file):
-    read_json(file_name)
 
--- methods.py
+++ methods.py
@@ -2,6 +2,8 @@ import math
 import random
 import json
 
+import matplotlib.pyplot as plt
+
 from functions import read_json, Write_to_json
 
 
@@ -21,10 +23,14 @@ def Single_keyword_All_Year(file_dir):
     # replace all keywords with accumulative counts in a shared dictionary whatever years
     All_dic = {}
     kw = read_json(file_dir)
+    sum_num = 0
 
     for w_dic in kw.values():
         key_lis = w_dic.keys()
         count_lis = w_dic.values()
+
+        # cumulate all counts
+        sum_num += sum(count_lis)
 
         for k,c in zip(key_lis, count_lis):
             
@@ -37,17 +43,16 @@ def Single_keyword_All_Year(file_dir):
                 old_count = int(All_dic[k])
                 new_count = old_count + int(c)
                 All_dic.update({k:new_count})
-    return All_dic
+    return All_dic, sum_num
 
 
 def Single_keyword_Sort_Filter_All_Year(file_dir, display_threshold):
     # Acquire the comprehensive dictionary of sampled data
-    whole_dict = Single_keyword_All_Year(file_dir)
+    whole_dict, sum_count = Single_keyword_All_Year(file_dir)
 
     # Descendingly sort the whole keyword dictionary by their frequences, Pick top 20 to form a new dictionary
     sorted_whole_dic = {k: v for k, v in sorted(whole_dict.items(), key=lambda item: item[1], reverse = True)[:int(display_threshold)]}
-
-    return sorted_whole_dic
+    return sorted_whole_dic, sum_count
 
 
 def Add_to_tabu_json(word, json_file):
@@ -56,13 +61,12 @@ def Add_to_tabu_json(word, json_file):
     dic = d['Tabu']
 
     if word in d['Tabu']:
-        print("The word %s already exists"%word)
+        print("The word '%s' already exists"%word)
 
     else:
         dic.append(word)
         d.update({'Tabu':dic})
         print("Succeed! The new word '%s' has been add to excluding list"%word)
-
     Write_to_json(json_file, d)
 
 
@@ -75,12 +79,12 @@ def Add_to_replace_json(word, main_word, json_file):
         exis_lis = replace_dic[main_word]
         exis_lis.append(word)
         replace_dic.update({main_word:exis_lis})
-        print("Succefully added %s to existing mainword '%s'"%(word, main_word))
+        print("Succefully added '%s' to existing mainword '%s'"%(word, main_word))
     
     # Create new mainword
     else:
         replace_dic.update({main_word:[word]})
-        print("Succefully created %s as a new mainwords and newly added '%s'"%(main_word, word))
+        print("Succefully created '%s' as a new mainwords and newly added '%s'"%(main_word, word))
 
     d.update({'Replacement':replace_dic})
 
