c0102968249bd7fecba4794ee928ea1f252efa10
Qiming YE <34636290+kimiye1989@users.noreply.github.com>
2022-10-28 10:13:49 +0100

    functions

--- functions.py
+++ functions.py
@@ -3,4 +3,12 @@ import json
 
 import pandas as pd
 
+def read_json(file_name):
+    with open(file_name, 'r') as f:
+        data = json.load(f)
+    return data
+
+def Write_to_json(file_name, data):
+    with open(file_name, 'w', encoding='utf-8') as f:
+        json.dump(data, f, ensure_ascii=False, indent=4)
 
